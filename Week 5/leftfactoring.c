eb9a7ab83734d4a4ce804652e4ec427068faa3de
gomuningen2004 <govardhan.personal.new@gmail.com>
2024-03-16 18:41:51 +0530

    mistake rectified

--- Week 5/leftfactoring.c
+++ Week 5/leftfactoring.c
@@ -1,44 +1,61 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
 
-int main() {
-    char prod[20][20], alpha[20][20], beta[20][20];
-    int i, j, k, n;
-    
-    printf("Enter the number of productions: ");
-    scanf("%d", &n);
-    printf("Enter %d productions:\n", n);
-    getchar();
-    
-    for(k = 1; k <= n; k++) {
-        fgets(prod[k], sizeof(prod[k]), stdin);
-        prod[k][strlen(prod[k]) - 1] = '\0';
-    }
-    
-    for(k = 1; k <= n; k++) {
-        if(prod[k][0] == prod[k][3]) {
-            printf("\nProduction %d is left recursive\n", k);
+void removeLeftFactoring(char prod[20][20], int n) {
+    char alpha[10][10], beta1[10][10], beta2[10][10];
+    int i, j, k;
+
+    for (int x = 0; x < n; x++) {
+        if (prod[x][2] == '-') {
             i = 4;
             j = 0;
-            while(prod[k][i] != '/') {
-                alpha[k][j++] = prod[k][i++];
+
+            while (prod[x][i] != '/' && prod[x][j] != '\0' && prod[x][i] == prod[x][j]) {
+                alpha[x][j] = prod[x][i];
+                i++;
+                j++;
             }
-            alpha[k][j] = '\0';
-            
-            i = i + 1;
-            j = 0;
-            while(prod[k][i] != '\0') {
-                beta[k][j++] = prod[k][i++];
+            alpha[x][j] = '\0';
+
+            k = 0;
+            while (prod[x][i] != '/' && prod[x][i] != '\0') {
+                beta1[x][k++] = prod[x][i++];
             }
-            beta[k][j] = '\0';
-            
-            printf("Production %d after removing the left recursion is:\n", k);
-            printf("%c->%s%c'\n", prod[k][0], beta[k], prod[k][0]);
-            printf("%c'->%s%c'/$\n", prod[k][0], alpha[k], prod[k][0]);
+            beta1[x][k] = '\0';
+
+            k = 0;
+            if (prod[x][i] == '/') {
+                i++;
+                while (prod[x][i] != '\0') {
+                    beta2[x][k++] = prod[x][i++];
+                }
+                beta2[x][k] = '\0';
+            } else {
+                beta2[x][0] = '$';
+                beta2[x][1] = '\0';
+            }
+
+            printf("\n%c->%s%c'\n", prod[x][0], alpha[x], prod[x][0]);
+            printf("%c'->%s/%s\n", prod[x][0], beta1[x], beta2[x]);
         } else {
-            printf("Production %d is not left recursive\n", k);
+            printf("The production %s is not in the correct format.\n", prod[x]);
         }
     }
+}
+
+int main() {
+    char prod[20][20];
+    int n;
+
+    printf("Enter the number of productions: ");
+    scanf("%d", &n);
+
+    printf("Enter the %d productions: \n", n);
+    getchar(); // Consume newline character left in buffer
+    for (int i = 0; i < n; i++) {
+        fgets(prod[i], 20, stdin);
+    }
+
+    removeLeftFactoring(prod, n);
+
     return 0;
 }
