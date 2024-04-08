408a4d46832f1cd25b643e04c46a38da987f490a
gomuningen2004 <govardhan.personal.new@gmail.com>
2024-04-08 08:37:40 +0530

    name changes

--- Lab Mid 1 programs/lex.yy.c
+++ Lab Mid 1 programs/lex.yy.c
@@ -282,21 +282,21 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
-static yyconst short int yy_accept[8] =
+#define YY_NUM_RULES 1
+#define YY_END_OF_BUFFER 2
+static yyconst short int yy_accept[6] =
     {   0,
-        0,    0,    4,    2,    1,    1,    0
+        0,    0,    2,    1,    0
     } ;
 
 static yyconst int yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -321,29 +321,29 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[4] =
+static yyconst int yy_meta[2] =
     {   0,
-        1,    1,    2
+        1
     } ;
 
-static yyconst short int yy_base[9] =
+static yyconst short int yy_base[7] =
     {   0,
-        0,    0,    5,    6,    0,    0,    6,    2
+        0,    0,    2,    3,    3,    0
     } ;
 
-static yyconst short int yy_def[9] =
+static yyconst short int yy_def[7] =
     {   0,
-        7,    1,    7,    7,    8,    8,    0,    7
+        6,    6,    5,    5,    0,    5
     } ;
 
-static yyconst short int yy_nxt[10] =
+static yyconst short int yy_nxt[5] =
     {   0,
-        4,    4,    5,    6,    7,    3,    7,    7,    7
+        4,    5,    3,    5
     } ;
 
-static yyconst short int yy_chk[10] =
+static yyconst short int yy_chk[5] =
     {   0,
-        1,    1,    1,    8,    3,    7,    7,    7,    7
+        6,    3,    5,    5
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -360,22 +360,21 @@ char *yytext;
 #line 1 "prime.l"
 #define INITIAL 0
 #line 2 "prime.l"
-#include <stdio.h>
-#include <stdbool.h>
+    #include <stdio.h>
+    #include <stdbool.h>
 
-bool isprime(int num) {
-if (num <= 1) {
-return false;
-}
-for (int i = 2; i * i <= num; ++i) {
-if (num % i == 0) {
-return false;
-}
-}
-return true;
-}
-
-#line 379 "lex.yy.c"
+    bool isprime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i; ++i) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+#line 378 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -526,9 +525,19 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 19 "prime.l"
+#line 18 "prime.l"
 
-#line 532 "lex.yy.c"
+    [0-9]+ {
+        int num = atoi(yytext);
+        if (isprime(num)) {
+            printf("%d is a prime number.\n", num);
+        } else {
+            printf("%d isn't a prime number.\n", num);
+        }
+    }
+
+    .|\n { }
+#line 541 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -579,13 +588,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 8 )
+				if ( yy_current_state >= 6 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 3 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -613,28 +622,10 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 20 "prime.l"
-{
-int num = atoi(yytext);
-if (isprime(num)) {
-printf("%d is a prime number.\n", num);
-} else {
-printf("%d isn't a prime number.\n", num);
-}
-printf("Enter a number: ");
-}
-	YY_BREAK
-case 2:
-YY_RULE_SETUP
-#line 30 "prime.l"
-{ }
-	YY_BREAK
-case 3:
-YY_RULE_SETUP
-#line 31 "prime.l"
+#line 29 "prime.l"
 ECHO;
 	YY_BREAK
-#line 638 "lex.yy.c"
+#line 629 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -926,7 +917,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 8 )
+			if ( yy_current_state >= 6 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -961,11 +952,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 8 )
+		if ( yy_current_state >= 6 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 7);
+	yy_is_jam = (yy_current_state == 5);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1520,15 +1511,10 @@ int main()
 	return 0;
 	}
 #endif
-#line 31 "prime.l"
+#line 29 "prime.l"
 
 
 int main() {
-printf("Enter a number: ");
-yylex();
-return 0;
-}
-
-int yywrap() {
-return 1;
+    yylex();
+    return 0;
 }
